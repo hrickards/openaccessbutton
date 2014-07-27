99815405524524e17afc9d3fc09e6f15793d4a18
Harry Rickards <hrickards@gmail.com>
2014-07-28 00:40:39 +0100

    Basic (text-only) XML parser to show advocacy content

--- app/src/main/java/org/openaccessbutton/openaccessbutton/AdvocacyFragment.java
+++ app/src/main/java/org/openaccessbutton/openaccessbutton/AdvocacyFragment.java
@@ -9,9 +9,17 @@ package org.openaccessbutton.openaccessbutton;
 
 import android.os.Bundle;
 import android.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import org.openaccessbutton.openaccessbutton.advocacy.XmlParser;
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * Shows static/semi-dynamic content (regarding open access advocacy) in a WebView.
@@ -25,6 +33,24 @@ public class AdvocacyFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_content, container, false);
+        View view = inflater.inflate(R.layout.fragment_advocacy, container, false);
+
+        // XML file containing content
+        InputStream object = this.getResources().openRawResource(R.raw.advocacy);
+        // LinearLayout to append content to
+        LinearLayout layout = (LinearLayout) view.findViewById(R.id.advocacy_content);
+
+        try {
+            XmlParser xmlParser = new XmlParser();
+            xmlParser.parseToView(object, layout, getActivity().getApplicationContext());
+        } catch (IOException e) {
+            // TODO: Do something
+            Log.e("openaccess", "exception", e);
+        } catch (XmlPullParserException e) {
+            // TODO: Do something
+            Log.e("openaccess", "exception", e);
+        }
+
+        return view;
     }
 }
--- app/src/main/java/org/openaccessbutton/openaccessbutton/BrowserFragment.java
+++ app/src/main/java/org/openaccessbutton/openaccessbutton/BrowserFragment.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (C) 2014 Open Access Button
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+
 package org.openaccessbutton.openaccessbutton;
 
 import android.os.Bundle;
