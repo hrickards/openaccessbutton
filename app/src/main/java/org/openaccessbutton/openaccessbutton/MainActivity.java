390edfc9cbe1a6553b510afbcdf4689849a77b6c
vesko95 <edv_junior@abv.bg>
2014-08-07 17:54:33 +0100

    Lots of work

--- app/src/main/java/org/openaccessbutton/openaccessbutton/MainActivity.java
+++ app/src/main/java/org/openaccessbutton/openaccessbutton/MainActivity.java
@@ -18,6 +18,7 @@ import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.webkit.WebView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -52,9 +53,7 @@ public class MainActivity extends Activity implements OnFragmentNeededListener,
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         initialiseNavigation();
-
         // Show the default fragment
         switchToFragment(0);
     }
--- app/src/main/java/org/openaccessbutton/openaccessbutton/browser/BrowserFragment.java
+++ app/src/main/java/org/openaccessbutton/openaccessbutton/browser/BrowserFragment.java
@@ -12,7 +12,10 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.view.KeyEvent;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.EditText;
 import org.openaccessbutton.openaccessbutton.R;
 
 /**
@@ -20,15 +23,39 @@ import org.openaccessbutton.openaccessbutton.R;
  * them to OAB if they're paywalled.
  * TODO: Implement
  */
+
 public class BrowserFragment extends Fragment {
     public BrowserFragment() {
         // Required empty public constructor
     }
 
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_browser, container, false);
+        View view = inflater.inflate(R.layout.fragment_browser, container, false);
+        WebView mWebView = (WebView)view.findViewById(R.id.mWebView);
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.setWebViewClient(new WebViewClient());
+        mWebView.loadUrl("http://www.google.com");
+        return view;
     }
+
+
+    WebView mWebView;
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if(event.getAction() == KeyEvent.ACTION_DOWN){
+            switch(keyCode)
+            {case KeyEvent.KEYCODE_BACK:
+                    if(mWebView.canGoBack()){
+                        mWebView.goBack();
+                    }else{
+                        break;
+                    }
+                    return true;
+            }
+
+        }
+        return true;
+    }
+
 }
